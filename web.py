10728d732b325e966372bec75ff429283e8d89d4
ntyr10086 <linda0909580880@gmail.com>
2026-05-19 10:07:29 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -45,8 +45,15 @@ def index():
     link += "<a href=/road>十大肇事路口</a><hr>"
     link += "<a href=/weather>查詢天氣</a><hr>"
     link += "<a href=/rate>本週新片進DB</a><hr>"
+    link += "<a href=/demo>電影機器人</a><hr>"
 
     return link  
+
+
+
+@app.route("/demo")
+def course():
+    return render_template("demo.html")
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
